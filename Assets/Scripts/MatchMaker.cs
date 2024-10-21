afe128840d611111b40222e153a884d1ffe78e99
IdleNotIdle <idlenotidlegames@gmail.com>
2024-10-21 22:10:32 +0200

    Update #2
    Updated the mechanics, added attack and individual camera for each player. Also added the address enter fields for connecting.

--- Assets/Scripts/MatchMaker.cs
+++ Assets/Scripts/MatchMaker.cs
@@ -17,16 +17,126 @@ using Unity.Services.Relay.Models;
 public class MatchMaker : MonoBehaviour
 {
     public TextMeshProUGUI updateText;
+    public GameObject networkUI;
+    public GameObject joinUi;
+    public GameObject hostUi;
+    public GameObject mainCamera;
+    public GameObject cameraPrefab;
 
-    public void Host()
+    public TMP_InputField joinAddress;
+    public TMP_InputField hostAddress;
+
+    public new List<GameObject> cameraLists;
+
+    public UnityTransport.ConnectionAddressData ConnectionData;
+
+    public static MatchMaker instance;
+
+    private void Start()
     {
-        NetworkManager.Singleton.StartHost();
-        updateText.text = "I am Host";
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
-    public void Join()
+    private void AddressLog()
     {
+        Debug.Log("Address: " + ConnectionData.Address);
+        updateText.text = "Address: " + ConnectionData.Address;
+    }
+
+    public void HostUI()
+    {
+        networkUI.SetActive(false);
+        hostUi.SetActive(true);
+    }
+
+    public void HostBtn()
+    {
+        hostUi.SetActive(false);
+        Host(hostAddress.text);
+    }
+
+    public void Host(string address)
+    {
+        ConnectionData.Address = address;
+        NetworkManager.Singleton.StartHost();
+        updateText.text = "I am Host";
+        networkUI.SetActive(false);
+
+        Invoke("AddressLog", 1f);
+    }
+
+    public void JoinUI()
+    {
+        networkUI.SetActive(false);
+        joinUi.SetActive(true);
+    }
+
+    public void JoinBtn()
+    {
+        joinUi.SetActive(false);
+        Join(joinAddress.text);
+    }
+
+    public void Join(string address)
+    {
+        ConnectionData.Address = address;
         NetworkManager.Singleton.StartClient();
         updateText.text = "I am Client";
+        networkUI.SetActive(false);
+
+        Invoke("AddressLog", 1f);
+    }
+
+    public void CameraAttach(GameObject player, int ownerId)
+    {
+        if (ownerId == 0)
+        {
+            mainCamera.transform.SetParent(player.transform);
+            player.GetComponent<PlayerController>().hasCam = true;
+            cameraLists.Add(mainCamera);
+        }
+        else
+        {
+            GameObject newCam = Instantiate(cameraPrefab);
+            cameraLists.Add(newCam);
+            cameraLists[ownerId].transform.SetParent(player.transform);
+            player.GetComponent<PlayerController>().hasCam = true;
+
+            for (int i = 0; i < cameraLists.Count; i++)
+            {
+                if(i != ownerId)
+                {
+                    cameraLists[i].GetComponentInChildren<Camera>().enabled = false;
+                }
+                else
+                {
+                    cameraLists[i].GetComponentInChildren<Camera>().enabled = true;
+                }
+            }
+
+        }
+    }
+
+    public void UpdateCamera(int ownerId)
+    {
+        for (int i = 0; i < cameraLists.Count; i++)
+        {
+            if (i != ownerId)
+            {
+                cameraLists[i].GetComponentInChildren<Camera>().enabled = false;
+            }
+            else
+            {
+                cameraLists[i].GetComponentInChildren<Camera>().enabled = true;
+            }
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,5 @@
+using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
@@ -6,13 +8,43 @@ using UnityEngine;
 public class PlayerController : NetworkBehaviour
 {
     public Rigidbody2D PlayerRigidBody;
+    public Animator weaponAnim;
 
     public float moveSpeed;
+    public float attackRange;
+    public float attackRate;
+
+    private float _LastAttackTime;
+
+    private int _OwnerID;
+
+    [HideInInspector]
+    public bool me, hasCam;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(IsOwner)
+        {
+            me = true;
+            hasCam = false;
+        }
+        else
+        {
+            me = false;
+        }
+
+        _OwnerID = Convert.ToInt32(OwnerClientId.ToString());
+
+        Invoke("SetCamera", 0.5f);
+    }
+
+    private void SetCamera()
+    {
+        if(!hasCam)
+        {
+            MatchMaker.instance.CameraAttach(this.gameObject, _OwnerID);
+        }
     }
 
     // Update is called once per frame
@@ -20,6 +52,48 @@ public class PlayerController : NetworkBehaviour
     {
         if(!IsOwner) return;
 
+        if(hasCam)
+        {
+            MatchMaker.instance.UpdateCamera(_OwnerID);
+        }
+
+        Move();
+
+        if(Input.GetMouseButtonDown(0) && Time.time - _LastAttackTime > attackRate)
+        {
+            Attack();
+        }
+
+        float mouseX = (Screen.width/2) - Input.mousePosition.x;
+
+        if(mouseX < 0)
+        {
+            weaponAnim.transform.parent.localScale = new Vector3(1,1,1);
+        }
+        else
+        {
+            weaponAnim.transform.parent.localScale = new Vector3(-1,1,1);
+        }
+    }
+
+    void Move()
+    {
         PlayerRigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * (moveSpeed);
+    }
+
+    void Attack()
+    {
+        _LastAttackTime = Time.time;
+
+        Vector3 dir = (Input.mousePosition - Camera.main.ScreenToWorldPoint(transform.position)).normalized;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + dir, dir, attackRange);
+
+        if(hit.collider != null && hit.collider.gameObject.CompareTag("Enemy"))
+        {
+
+        }
+
+        weaponAnim.SetTrigger("Attack");
     }
 }
